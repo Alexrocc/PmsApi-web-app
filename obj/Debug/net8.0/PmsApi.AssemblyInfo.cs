1c4e78b9ee3a446ced7e95a1711c3fad2ecf5251
Alexrocc <alerocc70@gmail.com>
2024-08-27 17:12:16 +0200

    added Identity package

--- obj/Debug/net8.0/PmsApi.AssemblyInfo.cs
+++ obj/Debug/net8.0/PmsApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PmsApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c1da8b8893cb61f0ee0bf3381e90899f15d4181e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d9124b374b63b99641f9c43155ace21c24f37c5d")]
 [assembly: System.Reflection.AssemblyProductAttribute("PmsApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PmsApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
